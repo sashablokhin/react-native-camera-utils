d933d8c7a460e400a8daaf2834834eb9d8000cce
Alexander Blokhin <sashablokhindev@gmail.com>
2020-07-09 16:43:08 +0300

    Added maxResolution method

--- ios/CameraUtils.swift
+++ ios/CameraUtils.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import AVFoundation
+import UIKit
+import GPUImage
 
 @objc(CameraUtils)
 class CameraUtils: NSObject {
@@ -19,6 +22,29 @@ class CameraUtils: NSObject {
                     resolve: @escaping RCTPromiseResolveBlock,
                     rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
         
-        resolve("123x456")
+        let size = getCaptureResolution();
+        resolve(["width": size.width, "height": size.height])
+    }
+    
+    
+    func getCaptureResolution() -> CGSize {
+        var resolution = CGSize(width: 0, height: 0)
+        
+        let portraitOrientation = (UIScreen.main.bounds.height > UIScreen.main.bounds.width)
+        
+        let cameraDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let formatDescription = cameraDevice?.activeFormat.formatDescription {
+            let dimensions = CMVideoFormatDescriptionGetDimensions(formatDescription)
+            resolution = CGSize(width: CGFloat(dimensions.width), height: CGFloat(dimensions.height))
+        } else {
+            resolution = CGSize(width: CGFloat(UIScreen.main.bounds.width), height: CGFloat(UIScreen.main.bounds.height))
+        }
+        
+        if (!portraitOrientation) {
+            resolution = CGSize(width: resolution.height, height: resolution.width)
+        }
+        
+        return resolution
     }
 }
